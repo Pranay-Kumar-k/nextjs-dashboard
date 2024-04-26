d44803e9c108ab1934d73f4f9efb5b730b12649f
Pranay Kumar <pranay.satya@anywhere.co>
2024-04-27 01:35:11 +0530

    added noStore for demonstration of static rendering

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -18,10 +18,10 @@ export default async function Page() {
                 <Card title="Pending" value={totalPendingInvoices} type="pending" />
                 <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
                 <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        />
+                    title="Total Customers"
+                    value={numberOfCustomers}
+                    type="customers"
+                />
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
                 <RevenueChart revenue={revenue} />
